7221d599cefc1d52eb8a9774fd627c1bfe3c64a4
Joel Rodrigues <45329484+pantsufan@users.noreply.github.com>
2020-03-26 18:44:01 +0530

    Add files via upload

---
+++ check.js
@@ -0,0 +1,26 @@
+const base_url="https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise";
+fetch (`${base_url}`)
+    . then (response => {
+    return response.json();
+    })
+    . then (json => {
+        //console.log(json);
+        document.getElementById("last_refresh").innerHTML="<b> Last Refreshed At: </b>"+json.data.lastRefreshed
+        document.getElementById("total").innerHTML="<b> Total Cases: </b>"+json.data.total.confirmed;
+        document.getElementById("active").innerHTML="<b> Active Cases: </b>"+json.data.total.active;
+        document.getElementById("discharged").innerHTML="<b> Discharged: </b>"+json.data.total.recovered;
+        document.getElementById("dead").innerHTML="<b> Deaths: </b>"+json.data.total.deaths;
+    })
+
+const state_url="https://api.covid19india.org/state_district_wise.json";
+fetch (`${state_url}`)
+    . then (response => {
+    return response.json();
+    })
+    . then (json => {
+    //console.log(json);
+        document.getElementById("goa_totalcases").innerHTML="<b> Total Cases: </b>"+json.Goa.districtData['North Goa'].confirmed;
+        document.getElementById("goa_active").innerHTML="<b> Active Cases: </b>"+json.Goa.districtData['North Goa'].active;
+        document.getElementById("goa_recovered").innerHTML="<b> Discharged: </b>"+json.Goa.districtData['North Goa'].recovered;
+        document.getElementById("goa_deaths").innerHTML="<b> Deaths: </b>"+json.Goa.districtData['North Goa'].deaths;
+    })
